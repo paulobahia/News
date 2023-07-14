ebe6841fdb6d726411a5325c80388b27517d4399
Paulo Bahia <pauloipatinga22@gmail.com>
2023-07-14 00:56:01 -0300

    feat: Alteração visuais simples

--- src/screens/Home/components/FeedNews/index.tsx
+++ src/screens/Home/components/FeedNews/index.tsx
@@ -47,38 +47,28 @@ const FeedNews: React.FC<FeedNewsProps> = ({ navigation }) => {
             <View>
                 <ScrollView showsHorizontalScrollIndicator={false} className="py-3 gap-x-2 shadow-black shadow-2xl" horizontal={true}>
                     {categoryNews.map((item, index) => (
-                        <TouchableOpacity disabled={item.name == category} onPress={() => setCategoryFeed(item.name)} key={index} className={`${category == item.name ? 'bg-background-primary' : 'bg-background-light/40'} rounded-md px-4 py-0.5 transition-colors `}>
-                            <Text className={`${category == item.name ? 'text-white font-nunito-bold' : 'font-nunito-medium text-neutral-200'}`}>{item.name}</Text>
+                        <TouchableOpacity disabled={item.name == category} onPress={() => setCategoryFeed(item.name)} key={index} className={`${category == item.name ? 'bg-background-primary' : 'bg-slate-500/20'}  shadow-2xl rounded-md px-4 py-0.5 transition-colors `}>
+                            <Text className={`${category == item.name ? 'text-white font-nunito-bold' : 'font-nunito-medium text-gray-300'}`}>{item.name}</Text>
                         </TouchableOpacity>
                     ))}
                 </ScrollView>
                 {data.map((news, index) => (
-                    <TouchableOpacity onPress={() => navigation.navigate('SheetNews')} key={index} className="my-1 bg-white rounded-lg flex shadow-black shadow-2xl">
-                        <View className="flex flex-row">
-                            <Image source={{ uri: news.imagePath }} className="w-1/3 rounded-l-lg" />
-                            <View className="flex gap-y-2 flex-1 p-2">
+                    <TouchableOpacity onPress={() => navigation.navigate('SheetNews')} key={index} className="my-1 rounded-lg flex shadow-black shadow-2xl">
+                        <View className="flex flex-row shadow-2xl">
+                            <Image source={{ uri: news.imagePath }} className="w-1/3  rounded-xl" />
+                            <View className="flex gap-y-1.5 flex-1 p-2">
                                 <View className="flex flex-row items-center justify-between ">
                                     <View>
-                                        <View className={`${returnCategoryColor(news.category)} px-1 py-0.5 rounded-md`}><Text className="text-xs text-white font-nunito-medium">{news.category}</Text></View>
-                                    </View>
-                                    <View>
-                                        <View className="flex flex-row gap-x-2 items-center">
-                                            <TouchableOpacity>
-                                                <Ionicons name="md-share-social-outline" size={18} color="grey" />
-                                            </TouchableOpacity>
-                                            <TouchableOpacity>
-                                                <FontAwesome name="bookmark-o" size={18} color="black" />
-                                            </TouchableOpacity>
-                                        </View>
+                                        <View className={`px-2 py-0.5 bg-slate-500/20 rounded-md`}><Text className="text-xs text-black/50 font-nunito-bold">{news.category}</Text></View>
                                     </View>
                                 </View>
                                 <View>
-                                    <Text numberOfLines={2} ellipsizeMode="tail" className="text-[11px] font-nunito-regular">
+                                    <Text numberOfLines={2} ellipsizeMode="tail" className="text-[12px] font-nunito-bold">
                                         {news.title}
                                     </Text>
                                 </View>
-                                <View>
-                                    <Text className="text-[10px] font-nunito-light text-neutral-800">{formatDate(news.dateReleased)}</Text>
+                                <View className="flex">
+                                    <Text className="text-[10px] font-nunito-regular text-neutral-800">{formatDate(news.dateReleased)}</Text>
                                 </View>
                             </View>
                         </View>
--- src/screens/SheetNews/index.tsx
+++ src/screens/SheetNews/index.tsx
@@ -1,5 +1,5 @@
-import { useRef } from 'react'
-import { Text, View, TouchableOpacity } from "react-native"
+import { useRef, useState } from 'react'
+import { View, Image } from "react-native"
 import { StackNavigationProp } from "@react-navigation/stack";
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -14,22 +14,31 @@ const SheetNews: React.FC<SheetNewsScreenProps> = (props) => {
     const sheetRef = useRef<BottomSheet>(null)
     const snapPoints = ['65%', '80%']
 
+    const [sheetHeight, setSheetHeight] = useState(35);
+
+    const handleSheetChanges = (newSheetHeight: number) => {
+        setSheetHeight(100 - parseInt(snapPoints[newSheetHeight]))
+    };
+
     return (
-        <GestureHandlerRootView className="bg-neutral-300 flex flex-1 justify-start pt-12 items-center">
-            <View>
-                {/* Imagem da Notícia */}
+        <GestureHandlerRootView className="bg-neutral-300 flex flex-1 justify-start items-center">
+            <View className='flex-1'>
+                <Image
+                    source={{ uri: 'https://f.i.uol.com.br/fotografia/2023/07/13/168929673064b09f5a7d0dd_1689296730_3x2_xl.jpg' }}
+                    className='w-screen h-2/5'
+                />
             </View>
             <BottomSheet
                 ref={sheetRef}
-                snapPoints={snapPoints}
+                snapPoints={['68%', '80%']}
+                onChange={handleSheetChanges}
             >
                 <BottomSheetView>
                     <View className='flex justify-center items-center h-full'>
-                        {/* Conteúdo da Notícia */}
                     </View>
                 </BottomSheetView>
             </BottomSheet>
-        </GestureHandlerRootView>
+        </GestureHandlerRootView >
     )
 }
 
